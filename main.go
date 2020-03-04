0d5ad61582e5a4b3d56de73f6bcf366324dbf9b1
Meer Sawood <msawood@localhost.localdomain>
2020-03-04 14:51:37 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+import (
+	"net/http"
+	"fmt"
+	"time"
+	"html/template"
+)
+
+//Create a struct that holds information to be displayed in our HTML file
+type Welcome struct {
+	Name string
+	Time string
+}
+
+//Go application entrypoint
+func main() {
+	
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+
+	
+	templates := template.Must(template.ParseFiles("templates/welcome.html"))
+
+	
+
+	http.Handle("/static/", //final url can be anything
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static")))) //Go looks in the relative static directory first, then matches it to a
+			
+	http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
+
+		//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name;
+		}
+		 
+		if err := templates.ExecuteTemplate(w, "welcome.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	
+	fmt.Println(http.ListenAndServe(":8080", nil));
+}
